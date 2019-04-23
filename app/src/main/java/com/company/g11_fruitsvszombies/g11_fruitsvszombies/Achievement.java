69959427cd78f76b7e7b5fe6ef157a4ad3453e6f
Raiyan Reza <raiyan-hk@hotmail.com>
2019-04-23 19:10:43 +0800

    added two images and changes to layout

--- app/src/main/java/com/company/g11_fruitsvszombies/g11_fruitsvszombies/Achievement.java
+++ app/src/main/java/com/company/g11_fruitsvszombies/g11_fruitsvszombies/Achievement.java
@@ -3,6 +3,8 @@ package com.company.g11_fruitsvszombies.g11_fruitsvszombies;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -19,6 +21,8 @@ public class Achievement extends AppCompatActivity {
 
         TextView textView = (TextView) findViewById(R.id.first_time_dead);
         TextView textView1 =(TextView) findViewById(R.id.Unlocked_Dominique);
+        ImageView dead = (ImageView) findViewById(R.id.dead);
+        ImageView samgor = (ImageView) findViewById(R.id.samgor);
 
         SharedPreferences setting = getSharedPreferences("Data",MODE_PRIVATE);
 
@@ -28,9 +32,14 @@ public class Achievement extends AppCompatActivity {
 
         if(firstDead == 10)
             textView.setText("Your first time dead");
+            dead.setVisibility(View.VISIBLE);
 
-        if(unlock_dominic == 12)
-            textView1.setText("Dominic Award");
+        if(unlock_dominic == 12){
+            textView1.setText("Sam Gor Phd Farmer Award");
+            samgor.setVisibility(View.VISIBLE);
+
+        }
+
 
 
     }
